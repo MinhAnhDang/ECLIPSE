5813f4e35dcc025b52566f84599164dd5603e8c5
minhanh.dang <dangminhanh64@gmail.com>
2024-06-21 00:22:28 +0700

    Update workaround.py

--- workaround.py
+++ workaround.py
@@ -7,23 +7,22 @@ from skimage import io
 def save_slice_as_grayscale(slice_data, output_dir, name):
     # Normalize the slice data to [0, 255]
     normalized_slice = ((slice_data - slice_data.min()) / (slice_data.max() - slice_data.min()) * 255).astype(np.uint8)
-
     # Save the grayscale slice as a PNG
     output_path = os.path.join(output_dir, f"{name}.jpg")
     io.imsave(output_path, normalized_slice)
 
 
-save_dir = "BTCV"
-training_image_root = "RawData/Training/img"
-training_label_root = "RawData/Training/label"
-training_image_paths = [os.path.join(training_image_root, f) for f in os.listdir(training_image_root)]
-training_label_paths = [os.path.join(training_label_root, f) for f in os.listdir(training_label_root)]
+save_dir = "/root/datasets/BTCV"
+training_image_root = "/root/datasets/RawData/Training/img"
+training_label_root = "/root/datasets/RawData/Training/label"
+training_image_paths = sorted([os.path.join(training_image_root, f) for f in os.listdir(training_image_root)])
+training_label_paths = sorted([os.path.join(training_label_root, f) for f in os.listdir(training_label_root)])
 mode = ["training", "validation"]
 print(training_image_paths, training_label_paths)
 image_dir = os.path.join(save_dir, "images")
 for index, file in enumerate(training_image_paths):
-    if index >= 35:
-        output_dir = os.path.join(image_dir, "validations")
+    if index >= 25:
+        output_dir = os.path.join(image_dir, "validation")
     else:
         output_dir = os.path.join(image_dir, "training")
     img  = nibabel.load(file)
@@ -47,15 +46,10 @@ def save_slice_as_grayscale(slice_data, output_dir, name):
     io.imsave(output_path, normalized_slice)
 
 
-save_dir = "BTCV"
-training_label_root = "RawData/Training/label"
-training_label_paths = [os.path.join(training_label_root, f) for f in os.listdir(training_label_root)]
-# mode = ["training", "validation"]
-print(training_image_paths, training_label_paths)
 label_dir = os.path.join(save_dir, "annotations")
 for index, file in enumerate(training_image_paths):
-    if index >= 35:
-        output_dir = os.path.join(label_dir, "validations")
+    if index >= 25:
+        output_dir = os.path.join(label_dir, "validation")
     else:
         output_dir = os.path.join(label_dir, "training")
     img  = nibabel.load(file)
